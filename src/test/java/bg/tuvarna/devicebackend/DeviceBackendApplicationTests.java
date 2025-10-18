6b579b3ae517a92869149325c98c79ebbf3ae7d4
MilenBogd <160730059+MilenBogd@users.noreply.github.com>
2025-10-18 22:07:42 +0300

    First Unity Tests

--- src/test/java/bg/tuvarna/devicebackend/DeviceBackendApplicationTests.java
+++ src/test/java/bg/tuvarna/devicebackend/DeviceBackendApplicationTests.java
@@ -1,13 +1,21 @@
 package bg.tuvarna.devicebackend;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DeviceBackendApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
     }
-
 }
